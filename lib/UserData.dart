42c3289d89e7ae52de20f75b3a695dcabbf17802
cporte1 <cporte1@student.gsu.edu>
2024-10-26 12:53:20 -0400

    added file for handling user data

---
+++ lib/UserData.dart
@@ -0,0 +1,28 @@
+class UserData {
+  final String name;
+  final String totalBalance;
+  final String income;
+  final String expense;
+  final List<Transactions> transactions;
+
+  const UserData({
+    required this.name,
+    required this.totalBalance,
+    required this.income,
+    required this.expense,
+    required this.transactions
+});
+}
+
+class Transaction {
+  final itemCategory category;
+  final TransactionType transactionType;
+  final String itemCategoryName;
+  final String itemName;
+  final String amount;
+  final String date;
+
+  const Transaction(this.category, this.transactionType, this.itemCategoryName, this.amount, this.date, this.itemName);
+}
+
+
